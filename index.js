91fe159106da3ccf1f9d77f9494027a443d04930
maneendra <katukuri.abhimani@gmail.com>
2022-09-08 10:46:09 +0000

    Congrats-Card

---
+++ index.js
@@ -0,0 +1,25 @@
+const element = (
+  <div class="bg-container1">
+    <h1 class="heading">Congratulations</h1>
+    <div class="bg-container2">
+      <div class="img-cont">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+          class="image"
+        />
+      </div>
+      <h1 class="heading2">Kiran V</h1>
+      <p class="paragraph">
+        Vishnu Institute of Computer Education and technology,Bhimavaram
+      </p>
+      <div class="img-cont">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+          class="image"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
